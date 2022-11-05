fd873b647e7aa0cb25f4cdb65339120faa3c9683
bhabesh kumar baro <111524590+bhabeshbaro@users.noreply.github.com>
2022-11-05 23:12:12 +0530

    new progam to upload

---
+++ code.py
@@ -0,0 +1,140 @@
+## not for children 
+
+
+
+print('enter two numbers whose sum is less than or equal to 100')
+
+num1=input("enter the first number:")
+num2=input("enter the second number:")
+
+sum=int(num1)+int(num2)
+print("the sum of num1 and num2: ",sum)
+
+if(sum == 0 ):
+    import webbrowser
+    import time
+    import random
+    sites =['google.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+    
+if(sum <= 10 ):
+    import webbrowser
+    import time
+    import random
+    
+    sites =('instagram.com')
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+if(10< sum <=20 ):
+    import webbrowser
+    import time
+    import random
+    
+    sites =['microsoft.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+    
+if(20 < sum <=30 ):
+    import webbrowser
+    import time
+    import random
+    
+    sites =['playstore.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+    if(30 < sum <=40 ):
+        import webbrowser
+    import time
+    import random
+    
+    sites =['facebook.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+if(40 < sum <=50 ):
+    import webbrowser
+    import time
+    import random
+    
+    sites =['mastercode']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+    if(50 < sum <=60 ):
+        import webbrowser
+    import time
+    import random
+    
+    sites =['spotify.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+if(60< sum <=70 ):
+    import webbrowser
+    import time
+    import random
+    
+    sites =['flipkart.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+if(70 < sum <=80):
+    import webbrowser
+    import time
+    import random
+    
+    sites =['clash of clans.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+if(80 < sum <=90):
+    import webbrowser
+    import time
+    import random
+    
+    sites =['dominos.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+    
+if(90 < sum <=100 ):
+    import webbrowser
+    import time
+    import random
+    
+    sites =['indian railway.com']
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+if (sum > 100):
+    print('ERROR!')
+    
+
+    
+    
+    
